80dbc6426af4fc24d58ad58b173174720f837137
nikolicdan <66527394+nikolicdan@users.noreply.github.com>
2020-12-11 18:29:44 +0100

    Add files via upload
    Pocetak kviza - unos imena, panel sa pravilima i dugme za pocetak igrice.

---
+++ hw-3/quiz.js
@@ -0,0 +1,10 @@
+$("p").hide();
+$("#start").hide();
+
+$("#next").click(function(){
+    $("b").hide();
+    $("#fname").hide();
+    $("#next").hide();
+    $("p").show();
+    $("#start").show();
+});
