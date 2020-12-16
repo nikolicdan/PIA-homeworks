f5a1dead74b3075b64fec7ac924d0ddba680b16b
nikolicdan <66527394+nikolicdan@users.noreply.github.com>
2020-12-16 23:26:12 +0100

    Update quiz.js
    Povezivanje JSON fajla sa JS.

--- hw-3/quiz.js
+++ hw-3/quiz.js
@@ -1,5 +1,7 @@
 $("p").hide();
 $("#start").hide();
+$("#next1").hide();
+$("#next2").hide();
 
 $("#next").click(function(){
     $("b").hide();
@@ -7,4 +9,17 @@ $("#next").click(function(){
     $("#next").hide();
     $("p").show();
     $("#start").show();
+    $("#start").click(function(){
+        fetch("quiz.json")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("demo").innerHTML = data.prvo;
+            $("#next1").show();
+            $("#start").hide();
+            $("#instructions").hide();
+            $("#next1").click(function(){
+                document.getElementById("demo").innerHTML = data.drugo;
+            })
+        })
+    })      
 });
