a098aebcf032c8ce884bebd82c304e9d3d4d690c
nikolicdan <66527394+nikolicdan@users.noreply.github.com>
2020-12-21 13:41:32 +0100

    Dodata sva pitanja sa ponudjenim odgovorima i unos odgovora sa dugmetom potvrdi.

--- hw-3/quiz.js
+++ hw-3/quiz.js
@@ -2,6 +2,12 @@ $("p").hide();
 $("#start").hide();
 $("#next1").hide();
 $("#next2").hide();
+$("#answer1").hide();
+$("#answer2").hide();
+$("#answer3").hide();
+$("#answer4").hide();
+$("#answer").hide();
+$("#confirm").hide();
 
 $("#next").click(function(){
     $("b").hide();
@@ -14,12 +20,114 @@ $("#next").click(function(){
         .then(response => response.json())
         .then(data => {
             document.getElementById("demo").innerHTML = data.prvo;
-            $("#next1").show();
+            document.getElementById("answer1").innerHTML = "1818.";
+            document.getElementById("answer2").innerHTML = "1852.";
+            document.getElementById("answer3").innerHTML = "1952.";
+            document.getElementById("answer4").innerHTML = "1847.";
+            $("#answer1").show();
+            $("#answer2").show();
+            $("#answer3").show();
+            $("#answer4").show();
             $("#start").hide();
             $("#instructions").hide();
-            $("#next1").click(function(){
+            $(".btn").click(function(){
                 document.getElementById("demo").innerHTML = data.drugo;
+                $("#answer").show();
+                $("#confirm").show();
+                $("#answer1").hide();
+                $("#answer2").hide();
+                $("#answer3").hide();
+                $("#answer4").hide();
+                $(".btn").click(function(){
+                    document.getElementById("demo").innerHTML = data.trece;
+                    document.getElementById("answer1").innerHTML = "1976.";
+                    document.getElementById("answer2").innerHTML = "1972.";
+                    document.getElementById("answer3").innerHTML = "1968.";
+                    document.getElementById("answer4").innerHTML = "1964.";
+                    $("#answer").hide();
+                    $("#confirm").hide();
+                    $("#answer1").show();
+                    $("#answer2").show();
+                    $("#answer3").show();
+                    $("#answer4").show();
+                    $(".btn").click(function(){
+                        document.getElementById("demo").innerHTML = data.cetvrto;
+                        $("#answer").show();
+                        $("#confirm").show();
+                        $("#answer1").hide();
+                        $("#answer2").hide();
+                        $("#answer3").hide();
+                        $("#answer4").hide();
+                        $(".btn").click(function(){
+                            document.getElementById("demo").innerHTML = data.peto;
+                            document.getElementById("answer1").innerHTML = "crkava";
+                            document.getElementById("answer2").innerHTML = "knjiga";
+                            document.getElementById("answer3").innerHTML = "insekata";
+                            document.getElementById("answer4").innerHTML = "rada";
+                            $("#answer").hide();
+                            $("#confirm").hide();
+                            $("#answer1").show();
+                            $("#answer2").show();
+                            $("#answer3").show();
+                            $("#answer4").show();
+                            $(".btn").click(function(){
+                                document.getElementById("demo").innerHTML = data.sesto;
+                                $("#answer").show();
+                                $("#confirm").show();
+                                $("#answer1").hide();
+                                $("#answer2").hide();
+                                $("#answer3").hide();
+                                $("#answer4").hide();
+                                $(".btn").click(function(){
+                                    document.getElementById("demo").innerHTML = data.sedmo;
+                                    document.getElementById("answer1").innerHTML = "Eni";
+                                    document.getElementById("answer2").innerHTML = "Leni";
+                                    document.getElementById("answer3").innerHTML = "Tina";
+                                    document.getElementById("answer4").innerHTML = "Dženi";
+                                    $("#answer").hide();
+                                    $("#confirm").hide();
+                                    $("#answer1").show();
+                                    $("#answer2").show();
+                                    $("#answer3").show();
+                                    $("#answer4").show();
+                                    $(".btn").click(function(){
+                                        document.getElementById("demo").innerHTML = data.osmo;
+                                        $("#answer").show();
+                                        $("#confirm").show();
+                                        $("#answer1").hide();
+                                        $("#answer2").hide();
+                                        $("#answer3").hide();
+                                        $("#answer4").hide();
+                                        $(".btn").click(function(){
+                                            document.getElementById("demo").innerHTML = data.deveto;
+                                            document.getElementById("answer1").innerHTML = "Barselona";
+                                            document.getElementById("answer2").innerHTML = "CSKA";
+                                            document.getElementById("answer3").innerHTML = "Olimpijakos";
+                                            document.getElementById("answer4").innerHTML = "Real Madrid";
+                                            $("#answer").hide();
+                                            $("#confirm").hide();
+                                            $("#answer1").show();
+                                            $("#answer2").show();
+                                            $("#answer3").show();
+                                            $("#answer4").show();
+                                            $(".btn").click(function(){
+                                                document.getElementById("demo").innerHTML = data.deseto;
+                                                $("#answer").show();
+                                                $("#confirm").show();
+                                                $("#answer1").hide();
+                                                $("#answer2").hide();
+                                                $("#answer3").hide();
+                                                $("#answer4").hide();
+                                            })
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
             })
         })
     })      
 });
+
